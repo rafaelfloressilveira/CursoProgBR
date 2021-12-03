9dccc5da39cd486c46a89c544501e7aa0fe4e056
rafaelfloressilveira <rafaelfloressilveira2@gmail.com>
2021-12-02 22:28:34 -0300

    Updade notes

---
+++ notes/01.JS/06.varConstLet/script.js
@@ -0,0 +1,18 @@
+// variáveis var, const e let
+
+// var = variável global, pode ser acessado de qualquer parte do código
+
+// const = constante, uma vez atribuído um valor, não pode ser reatribuido
+
+const numero = 1
+
+numero = 2 // Isto não pode ser feito, pois estou modificando a const
+
+// let → existe somente dentro do bloco de código { }
+
+const numero = [4] // Criando uma array
+
+numero.push(7); //Método push adiciona um número da array
+
+// resultado: numero = [4, 7]
+// eu altero as propriedades, mas não modifico o que é (um array)
