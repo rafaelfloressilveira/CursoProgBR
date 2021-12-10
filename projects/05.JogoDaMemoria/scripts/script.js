f423cb4b05dacb6147107be3d106f324cf772269
rafaelfloressilveira <rafaelfloressilveira2@gmail.com>
2021-12-10 00:45:11 -0300

    Memoriam game update

--- projects/05.JogoDaMemoria/scripts/script.js
+++ projects/05.JogoDaMemoria/scripts/script.js
@@ -1,26 +1,98 @@
-const FRONT = "card_front"
-const BACK = "card_back"
+const FRONT = "card_front";
+const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
+const TURN = "turn_card";
 
 let animals = [
-    "dog",
-    "koala",
-    "rabbit",
-    "elephant",
-    "cat",
-    "bird",
-    "duck",
-    "fish",
-    "frog",
-    "butterfly"];
+    "cao",
+    "coala",
+    "coelho",
+    "elefante",
+    "gato",
+    "passaro",
+    "pato",
+    "peixe",
+    "ra",
+    "borboleta"];
 
-createCardsFromAnimals(animals);
+
+let cards = null;
+
+startGame();
+
+function startGame() {
+    cards = createCardsFromAnimals(animals);
+    shuffleCards(cards);
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+
+    cards.forEach(card => {
+
+        let cardElement = document.createElement("div");
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener("click", flipCard);
+        gameBoard.appendChild(cardElement);
+
+    })
+
+
+
+}
+
+function createCardContent(card, cardElement) {
+
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+
+
+}
+
+function createCardFace(face, card, element) {
+
+    let cardElementFace = document.createElement("div");
+    cardElementFace.classList.add();
+    if(face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        let turnElement = document.createElement('img');
+        turnElement.classList.add(TURN);
+        turnElement.src = "./images/memoria.png";
+    }
+    element.appendChild(cardElementFace);
+}
+
+function shuffleCards(cards) {
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while(currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex]], cards[currentIndex] = cards[currentIndex], [cards[randomIndex]]
+    }
+}
+
 function createCardsFromAnimals(animals) {
 
     let cards = [];
-
-    for(let animals of animals) {
-        cards.push(createCardsFromAnimals(animals));
-    }
+    
+    animals.forEach((animals) => {
+        cards.push(createPairFromAnimals(animals));
+    })
 
     return cards.flatMap(pair => pair);
 
@@ -44,8 +116,8 @@ function createIdWithAnimals(animals) {
     return animals + parseInt(Math.random() * 1000);
 }
 
-{
-    Id
-    icon: "animals";
-    flipped: false;
+function flipCard() {
+
+this.classList.add("flip");
+
 }
