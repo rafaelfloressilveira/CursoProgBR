918eeee36445f1663057f8b162b009efd4be4b2a
rafaelfloressilveira <rafaelfloressilveira2@gmail.com>
2021-12-09 22:05:44 -0300

    Tic Tac Toe final version class

--- projects/04.JogoDaVelha/README.md
+++ projects/04.JogoDaVelha/README.md
@@ -1,6 +1,11 @@
-# Projeto Jogo da Velha
+## Projeto Jogo da Velha
 
-Aplicando os conceitos de HTML, CSS e JS - Projeto acompanhado pela aula
+# Aplicando os conceitos de HTML, CSS e JS - Projeto acompanhado pela aula
 
-# Alterações do projeto:
+## Desafios do projeto:
+
+# - [ ] Criar uma função de inicio de jogo
+# - [ ] Criar um botão para inicio do jogo
+# - [ ] Criar função para mostrar na tela quem é o jogador da vez
+# - [ ] Criar função para que cada jogador insira seu nome no jogo
 
--- projects/04.JogoDaVelha/scripts/interface.js
+++ projects/04.JogoDaVelha/scripts/interface.js
@@ -12,20 +12,18 @@ function handleClick(event) {
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if(handleMove(position)){
+        
+        setTimeout(() => {
+            alert("O Jogo Acabou - O vencedor foi " + playerTime );
+        }, 20);
+        
+    };
+    updateSquares(position);
 }
 
-function updateSquares() {
-
-    let squares = document.querySelectorAll(".square");
-
-    squares.forEach((square) => {
-        let position = square.id;
-        let symbol = board[position];
-
-        if (symbol != "") {
-            square.innerHTML = `<div class='${symbol}'></div></div>`
-        }
-    })
+function updateSquares(position) {
+    let square  =document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div></div>`;
 }
